c66c8e8a975527bf48e77618b2b70b0ed69c4a2e
Ataraxia <aixarata@proton.me>
2025-10-03 19:10:27 +0200

    added rendering of a player; ability to move with ESDF

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        screen.fill("black")
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 
