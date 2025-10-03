96d3ffd8f6e11d828a4fa4ae4297900a0389040a
Ataraxia <aixarata@proton.me>
2025-10-03 22:48:03 +0200

    added asteroid field with random asteroid spawns

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (drawables, updatables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables)
+    
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -15,13 +28,21 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for drawable in drawables:
+            drawable.draw(screen)
+
+        for updatable in updatables:
+            updatable.update(dt)
+
+        for asteroid in asteroids:
+            asteroid.draw(screen)
+            asteroid.update(dt)
+
         pygame.display.flip()
 
         clock.tick(60)
         dt = clock.tick(60) / 1000
-
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -4,6 +4,7 @@
 from constants import *
 from circleshape import *
 
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -19,7 +20,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(surface=screen, color="white", points=self.triangle(), width=2)
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
